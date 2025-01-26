1777b189682bcb9184413af68a66fdd7a1b99666
shakti-hub <shaktiswarupa142@gmail.com>
2025-01-26 23:03:26 +0530

    app started

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostComponentComponent } from './post/post-component/post-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/post/post-component/post-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-component',
+  standalone: false,
+  
+  templateUrl: './post-component.component.html',
+  styleUrl: './post-component.component.scss'
+})
+export class PostComponentComponent {
+
+}
